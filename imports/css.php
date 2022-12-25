6d1c17d49752a62b4d7c90a4bf2f2ee49d8e2a17
ishaninuwanthika <nuwanthikaishani@gmail.com>
2022-12-25 17:45:08 +0530

    don frontend web

---
+++ imports/css.php
@@ -0,0 +1,16 @@
+    <!-- Bootstrap CSS -->
+    <link href="<?php echo URL?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- font themify CSS -->
+    <link rel="stylesheet" href="<?php echo URL?>assets/css/themify-icons.css">
+    <!-- font awesome CSS -->
+    <link href="<?php echo URL?>assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <!-- stock CSS -->
+    <link href="<?php echo URL?>assets/css/animated-stock.css" rel="stylesheet">
+    <link href="<?php echo URL?>assets/css/owl.carousel.css" rel="stylesheet">
+    <link href="<?php echo URL?>assets/css/owl.theme.css" rel="stylesheet">
+    <link href="<?php echo URL?>assets/css/owl.transitions.css" rel="stylesheet">
+    <link href="<?php echo URL?>assets/css/stock-style.css?v=7" rel="stylesheet">
+    <link href="<?php echo URL?>assets/css/queries-stock.css?v=6" media="all" rel="stylesheet" type="text/css">
+    <link href="<?php echo URL?>assets/css/bg.css?v=5" rel="stylesheet">
+
+  
---
+++ imports/js.php
@@ -0,0 +1,8 @@
+ <!-- plugin JS -->
+ <script src="assets/plugin/pluginsstock.js" type="text/javascript"></script> 
+    <!-- stock JS -->
+    <script src="assets/js/stock.js" type="text/javascript"></script>
+
+    <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v15.0" nonce="SAWGfsvu"></script>
+
